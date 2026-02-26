de89775281c1f45dbbfa50def25d2b2d76402e43
TrishonParrilla <trishonp1@gmail.com>
2026-02-25 19:15:30 -0700

    Add initial implementation of task manager application with HTML, CSS, and JavaScript files

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+
+
+// An example of changing the logic execution
+
+function init ()
+
+{
+    console.log("Hello This is the DOM");
+
+}
+
+//forces my logic  my logic to run after the page is loaded
+window.onload = init;
