e65693a7756de6872674f23a3691e7b87c7e8bbd
TrishonParrilla <trishonp1@gmail.com>
2026-02-25 20:47:17 -0700

    added task class and add app styles with responsive design

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,14 @@
 
 // An example of changing the logic execution
 
+function hello() {
+    console.log("Hello World");
+}
 function init ()
 
 {
     console.log("Hello This is the DOM");
+    hello();
 
 }
 
